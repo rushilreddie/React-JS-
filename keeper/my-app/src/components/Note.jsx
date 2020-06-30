2d9446d81ee198a0cb70c0e9f03e414b1cf8101c
rushilreddie <rajarushil.reddy@gmail.com>
2020-06-30 16:20:11 -0500

    Notes application using React Base

---
+++ keeper/my-app/src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+    return (
+    <div className="note">
+    <h1>Title</h1>
+    <p>This is a sample paragraph.</p>
+    </div>
+    );
+}
+
+
+
+export default Note;
