7d898d95db183e6b6b7bb56f96887b43fa86aa50
rushilreddie <rajarushil.reddy@gmail.com>
2020-07-06 17:17:52 -0500

    Notes Keeper application mapping and adding notes dynamically

--- ES6 Mapping/src/index.js
+++ ES6 Mapping/src/index.js
@@ -2,10 +2,28 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
+// function double(x) {
+//     return x*2;
+// }
+
+// const newNumbers = numbers.map(double);
+// console.log(newNumbers);
+
+
 //Filter - Create a new array by keeping the items that return true.
+// var numbers = [];
+
+// function double(x){
+//     newNumbers.push(x*2);
+// }
+// numbers.forEach(double);
+
+console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
+
+
