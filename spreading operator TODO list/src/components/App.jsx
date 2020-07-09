f43f4c3e50201315a38778b0cf5563912db97427
rushilreddie <rajarushil.reddy@gmail.com>
2020-07-09 17:56:29 -0500

    todolist complete using component tree

--- spreading operator TODO list/src/components/App.jsx
+++ spreading operator TODO list/src/components/App.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea"
 
 
 function App() {
 
-const [inputText, setInputText]= useState("");
+
 const [items, setItems]= useState([]);
 
-function handleChange(event){
-  const newValue = event.target.value;
-  setInputText(newValue);
-}
 
-function addItem(){
+
+function addItem(inputText){
   setItems( (prevItems) =>{
     return [...prevItems, inputText];
   });
-  setInputText("");
+ 
+}
+
+function deleteItem(id){
+setItems((prevItems)=>{
+ return prevItems.filter((item, index)=>{
+    return index !== id;
+   });
+});
 }
 
   return (
@@ -24,17 +30,19 @@ function addItem(){
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value = {inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+
+<InputArea 
+  onAdd={addItem}
+/>
+
       <div>
         <ul>
-         { items.map((todoItem) => 
+         { items.map((todoItem, index) => 
          <ToDoItem 
+         key={index}
+         id={index}
            text={todoItem}
+           onChecked={deleteItem}
          />
          )}
         </ul>
